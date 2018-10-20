b2df1b989a6d9eecdf854534c36970781be67a95
venu-n <veejien@gmail.com>
2018-10-20 20:13:30 +0530

    new app

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,4 @@
 DEBUG = False
 SECRET_KEY = 'secret'
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = "sqlite:///../instance/flask-crud.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
