aaaa24a96f48c83f672d60b82b804c9c77b7f972
venu-n <veejien@gmail.com>
2018-10-21 12:11:34 +0530

    added index page/route

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -23,7 +23,10 @@ def register_user():
         
     return render_template('registration.html', form=form)
 
-@authentication.route('/', methods=['GET'])
+@authentication.route('/')
+def index():
+    return render_template('index.html')
+
 @authentication.route('/login', methods=['GET','POST'])
 def log_in_user():
     if current_user.is_authenticated:
